1241b414f6d3bce4a3e2ea808dc5a21fe36f2288
Sergey20062006 <ivanchenko.sergey@lll.kpi.ua>
2025-04-22 22:47:48 +0300

    last_1

--- 1.cpp
+++ 1.cpp
@@ -6,5 +6,8 @@ int main() {
     cout << "Hello World!" << endl;
     cout << "This is a C++ program." << endl;
     cout << "It prints a message to the console." << endl;
+    cout << "This is a simple example." << endl;
+    cout << "You can modify it as needed." << endl;
+    cout << "Have a great day!" << endl;
     return 0;
 }
