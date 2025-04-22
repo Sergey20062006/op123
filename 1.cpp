b6fa8037eeaec45efbb834f84658e7f460fa560e
Sergey20062006 <ivanchenko.sergey@lll.kpi.ua>
2025-04-22 22:42:16 +0300

    secfgh_5

--- 1.cpp
+++ 1.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     cout << "Hello World!" << endl;
     cout << "This is a C++ program." << endl;
+    cout << "It prints a message to the console." << endl;
     return 0;
 }
